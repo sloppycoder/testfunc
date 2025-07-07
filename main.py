21083565c254ca746191cce05a0fa4aa0ed9e102
Li Lin <guru.lin@gmail.com>
2025-07-07 18:59:21 +0800

    generated by cookiecutter

---
+++ main.py
@@ -0,0 +1,18 @@
+import logging
+import logging.config
+import yaml
+from pathlib import Path
+
+
+
+# Load the logging configuration
+LOGGING_CONFIG = {}
+with open(Path(__file__).parent / "logger_config.yaml", "r") as f:
+    LOGGING_CONFIG = yaml.safe_load(f)
+    logging.config.dictConfig(LOGGING_CONFIG)
+
+
+
+if __name__ == "__main__":
+    print("main.py")
+
